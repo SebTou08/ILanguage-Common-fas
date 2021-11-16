05a407361eadee9859841484fa00281c59281796
Sebastian Toulier <sebaas14toulier@gmail.com>
2021-11-15 20:06:52 -0500

    s

---
+++ src/main/java/pe/edu/upc/banking/common/application/RequestBodyType.java
@@ -0,0 +1,14 @@
+package pe.edu.upc.banking.common.application;
+
+public enum RequestBodyType {
+    VALID {
+        public String toString() {
+            return "VALID";
+        }
+    },
+    INVALID {
+        public String toString() {
+            return "INVALID";
+        }
+    },
+}
---
+++ src/main/java/pe/edu/upc/banking/common/application/ResultType.java
@@ -0,0 +1,6 @@
+package pe.edu.upc.banking.common.application;
+
+public enum ResultType {
+    SUCCESS,
+    FAILURE
+}
